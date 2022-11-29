13fe23a564baef19aa042d7f9039ade4e962b195
ponlawat koeisuwan <ponlawat6001@gmail.com>
2022-11-29 10:13:15 +0700

    start nuxt3

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <LogoApp />
+        <h1>Layout Default</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Index</h1>
+        <a-button type="primary" @click="showModal">Open Modal</a-button>
+        <a-modal v-model:visible="visible" title="Basic Modal" @ok="handleOk">
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        </a-modal>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import {  ref } from 'vue';
+    const visible = ref<boolean>(false);
+
+    const showModal = () => {
+        visible.value = true;
+    };
+
+    const handleOk = (e: MouseEvent) => {
+        console.log(e);
+        visible.value = false;
+    };
+
+</script>
+  
